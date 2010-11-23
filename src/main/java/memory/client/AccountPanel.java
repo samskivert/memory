d34773e21ceb9087088de6bc3347e9ffd748da0b
Michael Bayne <mdb@samskivert.com>
2010-11-23 14:13:18 -0800

    Improved the usability of the account page. Added some explanatory text to indicate to new users what a cortex is and how to appropriately name them.

--- src/main/java/memory/client/AccountPanel.java
+++ src/main/java/memory/client/AccountPanel.java
@@ -7,8 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.http.client.URL;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -16,6 +21,7 @@ import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
@@ -41,16 +47,26 @@ public class AccountPanel extends Composite
         _nickname.setTitle(data.userId);
         _logout.setHref(data.logoutURL);
 
+        _name.addKeyUpHandler(new KeyUpHandler() {
+            public void onKeyUp (KeyUpEvent event) {
+                processName();
+            }
+        });
+        _name.addChangeHandler(new ChangeHandler() {
+            public void onChange (ChangeEvent event) {
+                processName();
+            }
+        });
+
         addOwnedLinks(data.owned);
         addSharedLinks(data.shared);
 
         new MClickCallback<Void>(_create, _name) {
             protected boolean callService () {
-                _nname = _name.getText().trim();
-                if (_nname.length() < 4) {
-                    reportFailure(new Throwable("Cortex name must be at least 4 characters long."));
+                if (!processName()) {
                     return false;
                 }
+                _nname = _name.getText().trim();
                 _datasvc.createCortex(_nname, this);
                 return true;
             }
@@ -59,10 +75,12 @@ public class AccountPanel extends Composite
                 addOwnedLinks(data.owned);
                 Popups.infoBelow("Cortex created!", getPopupNear());
                 _name.setText("");
-                return true;
+                processName();
+                return false;
             }
             protected String _nname;
         };
+        _create.setEnabled(false);
     }
 
     protected void addOwnedLinks (List<String> cortexen)
@@ -98,6 +116,25 @@ public class AccountPanel extends Composite
         }
     }
 
+    protected boolean processName ()
+    {
+        String cid = _name.getText().trim().toLowerCase().replace(
+            ' ', '-').replaceAll("[&\\?]", "");
+        _name.setText(cid);
+        _create.setEnabled(cid.length() >= 4);
+        if (cid.length() == 0) {
+            _urltip.setText("");
+            return false;
+        } else if (cid.length() < 4) {
+            _urltip.setText("The name must be at least 4 characters long.");
+            return false;
+        } else {
+            _urltip.setHTML("Your cortex URL will be:<br/>\n" +
+                            "http://www.sparecortex.com/c/" + URL.encode(cid));
+            return true;
+        }
+    }
+
     protected interface Styles extends CssResource
     {
         String none ();
@@ -108,6 +145,7 @@ public class AccountPanel extends Composite
     protected @UiField Anchor _logout;
     protected @UiField FlowPanel _owned;
     protected @UiField FlowPanel _shared;
+    protected @UiField HTML _urltip;
 
     protected @UiField TextBox _name;
     protected @UiField Button _create;
