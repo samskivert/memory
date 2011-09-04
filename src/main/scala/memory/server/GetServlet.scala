b4c72fb59613382e546a14c61185c03c632bc883
Michael Bayne <mdb@samskivert.com>
2011-09-04 09:40:35 -0700

    Create a test cortex automatically when running in devmode.

--- src/main/scala/memory/server/GetServlet.scala
+++ src/main/scala/memory/server/GetServlet.scala
@@ -30,6 +30,15 @@ class GetServlet extends HttpServlet
     try {
       db.init
       _log.info("Database initialized.")
+
+      // create a test cortex if we're running in devmode
+      if (System.getProperty("com.google.appengine.runtime.environment") == "Development") {
+        if (MemoryLogic.createCortex("test", "none")) {
+          db.updateCortexPublicAccess("test", Access.WRITE)
+          _log.info("Created 'test' cortex. Test like the wind!")
+        }
+      }
+
     } catch {
       case e => println("Failed to init db " + e)
     }
